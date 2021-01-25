fa7719dfdf146cc8dc44fed1047285b1128d1c95
unknown <tuanpv29@gmail.com>
2021-01-25 17:13:06 +0700

    Add total tasks and style Todo List

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const todoTotal = document.querySelector('todo-total');
+const todoTotal = document.querySelector('.todo-total');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', closeDoneTodo);
+todoList.addEventListener('click', checkedCloseTodo);
 
 // Functions
 function addTodo(event) {
   // Prevent form from submitting
   event.preventDefault();
-  // Create todo div
-  const todoDiv = document.createElement('div');
-  todoDiv.classList.add('todo');
-  todoList.appendChild(todoDiv);
-  // Create todo-item li
   if (todoInput.value === '') {
     alert('You must write something!')
   } else {
+    // Create todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    todoList.appendChild(todoDiv);
+    // Create todo-item li
     const todoLi = document.createElement('li');
     todoLi.innerText = todoInput.value;
     todoLi.classList.add('todo-item');
@@ -34,23 +34,26 @@ function addTodo(event) {
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add('todo-trash');
     todoDiv.appendChild(trashButton);
-    // Append todoDiv to todoList
-    todoList.appendChild(todoDiv);
     // Clear todoInput value
     todoInput.value = '';
+    // Total item left
+    todoTotal.innerText = todoList.childElementCount;
   }
 }
 
-function closeDoneTodo(e) {
+function checkedCloseTodo(e) {
   const item = e.target;
-  // Delete todo
-  if (item.classList.item(0) === 'todo-trash') {
-    const close = item.parentElement;
-    close.remove();
-  };
+  var i = 0;
   // Checked todo
   if (item.classList.item(0) === 'todo-complete') {
-    const done = item.parentElement;
-    done.classList.toggle('completed');
+    const checkedTodo = item.parentElement.children[0];
+    checkedTodo.classList.toggle('completed');
   };
+  // Delete todo
+  if (item.classList.item(0) === 'todo-trash') {
+    const closeTodo = item.parentElement;
+    closeTodo.remove();
+  };
+  // Total item left
+  todoTotal.innerText = todoList.childElementCount - todoList.getElementsByClassName('completed').length;
 }
